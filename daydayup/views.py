62d8df4bdfb86c67ab88d5415028354bfce19439
heroH <herohu@shunshunliuxue.com>
2019-02-15 14:31:11 +0800

    test

--- daydayup/views.py
+++ daydayup/views.py
@@ -1,11 +1,15 @@
-from django.views.decorators.http import require_http_methods
-from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+from django.conf import settings
+
+import json
 
 
-@csrf_exempt
 @require_http_methods(['POST'])
 def ci(request):
     print(request)
-
-    return JsonResponse(dict(msg='版本更新成功'))
+    if getattr(request, 'HTTP_X_USER_AGENT', None) == settings.GIT_USER_AGENT and \
+            getattr(request, 'HTTP_X_GITHUB_EVENT', None) == settings.X_GIT_EVENT:
+        return JsonResponse(json.loads(request.body))
+    else:
+        return JsonResponse(dict(msg='版本更新失败'))
