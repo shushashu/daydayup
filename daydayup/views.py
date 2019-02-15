f09e64a0b9dc6483b716977461cba890562f40e1
heroH <herohu@shunshunliuxue.com>
2019-02-15 18:31:53 +0800

    ci test

--- daydayup/views.py
+++ daydayup/views.py
@@ -10,11 +10,15 @@ import os, sys
 @csrf_exempt
 @require_http_methods(['POST'])
 def ci(request):
-    print(request)
-    print(request.META.get('HTTP_USER_AGENT', None))
     if request.META.get('HTTP_USER_AGENT', None) == settings.GIT_USER_AGENT and \
             request.META.get('HTTP_X_GITHUB_EVENT', None) == settings.X_GIT_EVENT:
-        body = json.loads(str(request.body))
-        return JsonResponse(body)
+        body = json.loads(request.body.decode(encoding='utf8'))
+        # 检查是否满足版本更新条件
+        os.system('sh /%s/git_update.sh %s %s %s' % (
+            os.path.dirname(settings.BASE_DIR),
+            os.path.dirname(settings.BASE_DIR),
+            settings.BASE_DIR,
+            os.environ.get('VIRTUAL_ENV'),))
+        return JsonResponse(dict(msg='succeed'))
     else:
         return JsonResponse(dict(msg='error'))
