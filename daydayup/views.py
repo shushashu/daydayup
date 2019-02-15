a0959fd634bd1b49b08d32a29ac98eaa97f409bb
heroH <herohu@shunshunliuxue.com>
2019-02-15 14:39:16 +0800

    test

--- daydayup/views.py
+++ daydayup/views.py
@@ -1,10 +1,12 @@
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
+from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
 
 import json
 
 
+@csrf_exempt
 @require_http_methods(['POST'])
 def ci(request):
     if getattr(request, 'HTTP_X_USER_AGENT', None) == settings.GIT_USER_AGENT and \
